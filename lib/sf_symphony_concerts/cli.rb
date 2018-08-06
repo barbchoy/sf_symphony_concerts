d6a3804250a7d648dae75617c62143a01684aa00
Barbara Choy <barbchoy@gmail.com>
2018-08-06 04:33:12 +0000

    code improvement

--- lib/sf_symphony_concerts/cli.rb
+++ lib/sf_symphony_concerts/cli.rb
@@ -1,11 +1,5 @@
 class SfSymphonyConcerts::CLI
 
-  MONTHS_VALUES = {
-    one: '6',
-    two: '7',
-    three: '8',
-    four: '9'
-  }
 
   def call
     puts "Hello World!"
@@ -49,49 +43,19 @@ class SfSymphonyConcerts::CLI
     while input != "exit"
       puts "Select the month for the season of 2018-19 , type 17 for all, list or exit: "
       input = gets.strip.downcase
-
-      case input
-      when "1"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2018, 6)
-      when "2"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2018, 7)
-      when "3"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2018, 8)
-      when "4"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2018, 9)
-      when "5"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2018, 10)
-      when "6"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2018, 11)
-      when "7"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2018, 12)
-      when "8"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2019, 1)
-      when "9"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2019, 2)
-      when "10"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2019, 3)
-      when "11"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2019, 4)
-      when "12"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2019, 5)
-      when "13"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2019, 6)
-      when "14"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2019, 7)
-      when "15"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2019, 8)
-      when "16"
-        @concerts = SfSymphonyConcerts::Concert.this_month(2019, 9)
-      when "17"
+      if input.to_i >= 1 && input.to_i <= 7
+        @concerts = SfSymphonyConcerts::Concert.this_month(2018, input.to_i + 5)
+      elsif input.to_i >= 8 && input.to_i <= 16
+        @concerts = SfSymphonyConcerts::Concert.this_month(2019, input.to_i - 7)
+      elsif input.to_i == 17
         @concerts = SfSymphonyConcerts::Concert.this_month(3000, 100)
-      when "list"
+      elsif input == "list"
         list_months
       else
         puts "Not sure what you want, type list or exit "
       end
-
     end
+    
   end
 
   def goodbye
--- lib/sf_symphony_concerts/concert.rb
+++ lib/sf_symphony_concerts/concert.rb
@@ -1,5 +1,7 @@
 class SfSymphonyConcerts::Concert
 
+  attr_accessor :title, :date, :description, :conductor, :performers, :program
+
   def self.this_month(year, month)
     url_string = "http://www.sfsymphony.org/Buy-Tickets/Calendar#mo=#{year}:#{month}"
     puts url_string
@@ -12,7 +14,7 @@ class SfSymphonyConcerts::Concert
     concert_1.performers = "Violin: James Ehnes"
     concert_1.program = "Mendelssohn:Ruy Blas Overture\nBruch:Violin Concerto No. 1\nWagner:Overture and Venusberg Music from Tannhäuser\nWagner:Prelude and Liebestod from Tristan und Isolde"
 
-    concert_2.self.new
+    concert_2 = self.new
     concert_2.title = "Janowski Conducts Wagner’s Love Music"
     concert_2.date = "Thursday, May 2 - Saturday, May 4"
     concert_2.description = "Experience the passions of springtime in this romantic event at the symphony. The renowned conductor Marek Janowski conducts works by music’s master of sensuality, Richard Wagner. Hear Tannhäuser’s hedonistic adventure in the Venusberg followed by the enrapturing Liebestod from Tristan und Isolde. Plus, the “fierce and impressive” (San Francisco Classical Voice) James Ehnes turns up the bravado in Bruch’s rousing Violin Concerto No. 1."
@@ -20,23 +22,21 @@ class SfSymphonyConcerts::Concert
     concert_2.performers = "Performers:\nVolin: James Ehnes"
     concert_2.program = "Mendelssohn:Ruy Blas Overture\nBruch:Violin Concerto No. 1\nWagner:Overture and Venusberg Music from Tannhäuser\nWagner:Prelude and Liebestod from Tristan und Isolde"
 
-    concert_3.self.new
+    concert_3 = self.new
     concert_3.title = "MTT Conducts the Music of Debussy"
     concert_3.date = "Thursday, May 9 - Saturday, May 11"
     concert_3.description = "With one dreamy wisp of a piece, Debussy revolutionized music. Delicious and captivating, Prelude to The Afternoon of a Faun paved the way for colors and impressions never before expressed in sound. Luxuriate in the luminescent world of this and other iconic works, including the churning sea picture, La Mer. Also on the program, MTT welcomes one of today’s great musical visionaries, piano virtuoso Pierre-Laurent Aimard."
     concert_3.conductor = "Conductor: Michael Tilson Thomas"
     concert_3.performers = "Performers:\nPiano: Pierre-Laurent Aimard\nWomen of the San Francisco Symphony Chorus, Ragnar Bohlin, director\nSan Francisco Symphony"
-    concert_4.program = "Debussy: Prélude to The Afternoon of a Faun\nLigeti: Piano Concerto\nDebussy:Nocturnes\nDebussy:La Mer"
-  
-    concert_4.self.new
+    concert_3.program = "Debussy: Prélude to The Afternoon of a Faun\nLigeti: Piano Concerto\nDebussy:Nocturnes\nDebussy:La Mer"
+
+    concert_4 = self.new
     concert_4.title = "MTT Conducts the Music of Debussy"
     concert_4.date = "Thursday, May 9 - Saturday, May 11"
     concert_4.description = "With one dreamy wisp of a piece, Debussy revolutionized music. Delicious and captivating, Prelude to The Afternoon of a Faun paved the way for colors and impressions never before expressed in sound. Luxuriate in the luminescent world of this and other iconic works, including the churning sea picture, La Mer. Also on the program, MTT welcomes one of today’s great musical visionaries, piano virtuoso Pierre-Laurent Aimard."
     concert_4.conductor = "Conductor: Michael Tilson Thomas"
-    concert_4.performers = ""
-
-
-
+    concert_4.performers = "performers 3"
+    concert_4.program = "program 4"
 
     [concert_1, concert_2, concert_3, concert_4]
   end
